4a81418fa6ecc62d99d4481714db4741c36f8955
Akinola Kehinde <khennyakin@gmail.com>
2023-11-06 13:57:26 +0100

    all changes to api

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -12,3 +12,16 @@ exports.createStock = async (req, res, next) => {
     }
     
 }
+
+
+exports.getStocks = async(req, res, next) => {
+    try {
+        const Stock = await stock.find()
+        res.status(200).json({
+            success: true,
+            Stock
+        })
+    } catch (error){
+        res.status(400).json({error: error.message})
+    }
+}
--- src/routes/stockRoute.js
+++ src/routes/stockRoute.js
@@ -2,8 +2,9 @@ const express = require('express')
 
 const router = express.Router()
 
-const { createStock } = require('../controllers/stockController')
+const { createStock, getStocks } = require('../controllers/stockController')
 
 router.route('/create_stock').post(createStock)
+router.route('/get_stocks').get(getStocks)
 
 module.exports = router
